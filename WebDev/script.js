55cbf89e07dce220b537fb892a3748765fd06b9e
Toms <chen.tommy925@gmail.com>
2019-07-18 18:35:21 -0400

    assignments

---
+++ WebDev/script.js
@@ -0,0 +1,11 @@
+// 1) Select the section with an id of container without using querySelector.
+document.getElementById("container");
+
+// 2) Select the section with an id of container using querySelector.
+document.querySelector("#container");
+
+//3) Select all of the list items with a class of "second".
+var x = document.getElementsByClassName("second");
+
+// 4) Select a list item with a class of third, but only the list item inside of the ol tag.
+
