f24cfc8e57feaad86ae2f0e2187775b446eb5557
enterdoki <chen.tommy925@gmail.com>
2019-07-22 18:01:32 -0400

    intersection problem

--- CS/Week2/day2_intersectionarray.cpp
+++ CS/Week2/day2_intersectionarray.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -17,4 +18,14 @@ public:
             result.push_back(i);
         return result;
     }
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> unique;
+        for(int i = 0; i < nums.size(); i++) {
+            if(unique.find(nums[i]) != unique.end())
+                return true;
+            unique.insert(nums[i]);
+        }
+        return false;
+    }
 };
+
